21fd388247e33abf1534eb75466ea00b8abff50c
gavrigor <gavrigor@gmail.com>
2016-05-21 23:32:22 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
-# def application(environ, start_response):
-#    status = '200 OK'
-#    headers = [
-#    ('Content-Type', 'text/plain')
-#    ]
-#    body = environ['QUERY_STRING'].split("&")
-#    body = [item+"\n" for item in body]
-#    start_response(status, headers )
-#    return body 
+def application(environ, start_response):
+   status = '200 OK'
+   headers = [
+   ('Content-Type', 'text/plain')
+   ]
+   body = environ['QUERY_STRING'].split("&")
+   body = [item+"\n" for item in body]
+   start_response(status, headers )
+   return body 
 
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+# def app(environ, start_response):
+#    start_response('200 OK', [('Content-Type', 'text/plain')])
+#    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+#                  encoding="utf8")]
