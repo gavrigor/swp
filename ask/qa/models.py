e676e8b45cf4929f5958c99addeaa8fdd39c62df
gavrigor <gavrigor@gmail.com>
2016-05-22 22:41:19 +0300

    22.05.2016 22:40

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,8 +3,14 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class QuestionManager(models.Manager):
+  def new():
+    pass                                                            
+  def popular():                                                          
+    pass 
 
 class Question(models.Model):
+  objects = QuestionManager()
   title = models.CharField(default="", max_length=1024)
   text = models.TextField(default="")
   added_at = models.DateField(null=True)
