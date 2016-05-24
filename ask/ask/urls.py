be35ed25412622770150a7645a9823c1ac5cd9c2
gavrigor <gavrigor@gmail.com>
2016-05-24 23:31:48 +0300

    24/05/2016 23:00

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,13 +14,12 @@ include
 """ 
 from django.conf.urls import url, include 
 from django.contrib import admin 
-from ask.views import found, not_found
-# init25 
+from ask.views import found, not_found, init25 
 
 from qa.views import index, popular, ask, login_view, signup 
 urlpatterns = [
     url(r'^$', index),
-    #url(r'^init25/', init25),
+    url(r'^init25/', init25),
     url(r'^login/', login_view),
     url(r'^signup/', signup),
     url(r'^ask/', ask),
